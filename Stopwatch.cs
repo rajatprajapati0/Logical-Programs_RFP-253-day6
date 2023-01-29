7c9b4ba3fa337dd16d9573c21f5271ea5c2531cb
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-29 18:34:27 +0530

    Added : stopwatch file

---
+++ Stopwatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Stopwatch a = new Stopwatch();
+          
+
+
+            a.Start();
+
+            for (int i= 0;i<10;i++)
+             {
+                
+                Console.Write("");
+             }
+           
+              a.Stop();
+            Console.WriteLine(a.Elapsed);
+
+
+            
+
+        }
+    }
+}

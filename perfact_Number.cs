a47e3ae92af97ca2479edb9ec051cea2cd35c8fe
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-29 18:20:26 +0530

    Added: perfect_no. file

---
+++ perfact_Number.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Perfact_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter a no.");
+            int a = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i =1;i<=a/2;i++) 
+            {
+                if (a % i == 0)
+                {
+                    sum = i + sum;
+
+                   
+                }
+
+
+       
+            }
+
+            if (sum == a) 
+            {
+                Console.WriteLine(a+" is a perfact no.");
+            }
+            else
+            {
+                Console.WriteLine(a + " is not a perfact no.");
+            }
+
+
+            
+        }
+    }
+}

3cfa46fbabf0a36bba16601313fe1050ed31c075
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-29 18:27:47 +0530

    Added: reverse_no. file

---
+++ reverse_Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Reverse_a_Number
+{
+    internal class reverse_Number
+    {
+        static void Main(string[] args)
+        {
+            int revers= 0;
+            int rem;
+            int num = int.Parse(Console.ReadLine());
+            while (num != 0)                        
+            {                                       
+                rem = num % 10;                       
+
+                revers = revers * 10 + rem;        
+                num = num / 10;                    
+                    }
+            Console.WriteLine(revers);              
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

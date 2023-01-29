c075243eb47a4a4f9f3b00a5df6ffd2d519cc3b8
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-29 18:14:19 +0530

    Added:fibonacci_Series file

---
+++ fibonacci_Series.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci_Series
+{
+    internal class fibonacci_Series
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3 = 0;
+
+            for (int i = 0; i<20;i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3);
+                n1=n2;
+                n2=n3;
+
+            }
+           
+        }
+    }
+}
